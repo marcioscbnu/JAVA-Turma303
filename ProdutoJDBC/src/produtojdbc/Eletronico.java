3950f7284af7f716dbd6872716748c5f6b8e7a98
marcioscbnu <marcio.schoenfelder@edu.sc.senai.br>
2025-05-07 09:56:02 -0300

    Versão com Ajustes nas Consultas e implementação de parte da alteração

--- ProdutoJDBC/src/produtojdbc/Eletronico.java
+++ ProdutoJDBC/src/produtojdbc/Eletronico.java
@@ -12,86 +12,120 @@ import util.Conexao;
 
 public class Eletronico extends Produto {
 	private String marcaEletronico;
-	private int    garantiaMeses;
-	
+	private int garantiaMeses;
+
 	public Eletronico() {
-		
+
 	}
-	
-	public Eletronico(String codProduto, String nomeProduto, double precoProduto, 
-			          String marcaEletronico,int garantiaMeses) {
+
+	public Eletronico(String codProduto, String nomeProduto, double precoProduto, String marcaEletronico,
+			int garantiaMeses) {
 		super(codProduto, nomeProduto, precoProduto);
 		this.marcaEletronico = marcaEletronico;
 		this.garantiaMeses = garantiaMeses;
 	}
-	
+
 	public boolean incluirProduto() {
 		Connection con = Conexao.conectar();
-/*
+
 		// Incluindo Produto - inicio
-		String sql = "INSERT INTO produto values (?,?,?); ";		
+		String sql = "INSERT INTO produto values (?,?,?); ";
 		try {
 			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setString(1, this.codProduto);
-			stm.setString(2, this.nomeProduto);
-			stm.setDouble(3, this.precoProduto);
+			stm.setString(1, super.getCodProduto());
+			stm.setString(2, super.getNomeProduto());
+			stm.setDouble(3, super.getPrecoProduto());
 			stm.execute();
-		} catch (Exception e){
+		} catch (Exception e) {
 			if (e instanceof SQLIntegrityConstraintViolationException) {
-				System.out.println("Produto"
-						+ ""
-						+ ""
-						+ ""
-						+ ""
-						+ " Duplicado ! Verifique");	
+				System.out.println("Produto Duplicado ! Verifique");
 				return false;
 			} else if (e instanceof SQLException) {
 				System.out.println("Problemas ao inserir Produto ");
 				return false;
 			}
-			
+		}
+		// Incluindo Produto - inicio
+		String sql1 = "INSERT INTO eletronico values (?,?,?); ";
+		try {
+			PreparedStatement stm = con.prepareStatement(sql1);
+			stm.setString(1, super.getCodProduto());
+			stm.setString(2, this.marcaEletronico);
+			stm.setDouble(3, this.garantiaMeses);
+			stm.execute();
+		} catch (Exception e) {
+			if (e instanceof SQLIntegrityConstraintViolationException) {
+				System.out.println("Eletronico Duplicado ! Verifique");
+				return false;
+			} else if (e instanceof SQLException) {
+				System.out.println("Problemas ao inserir Eletronico ");
+				return false;
+			}
 		}
 
-		// Incluindo Produto - termino
-					*/	
-		return true;	
+		return true;
 
 	}
-	
-	
-	public List<Produto> listarProdutos(){
-		List<Produto> lstProd = new ArrayList<>();
+
+	public boolean alterarProduto() {
 		Connection con = Conexao.conectar();
-		lstProd = null;
+		String  sql = "UPDATE produto p ";
+				sql += "SET p.NOMEPRODUTO  = ?, ";
+				sql += "    p.PRECOPRODUTO = ? ";
+				sql += "WHERE p.CODPRODUTO = ? ;";
+		try {
+			PreparedStatement stm = con.prepareStatement(sql);
+			stm.setString(1, super.getNomeProduto());
+			stm.setDouble(2, super.getPrecoProduto());
+			stm.setString(3, super.getCodProduto());
+			stm.execute();
+		} catch (SQLException e) {
+			 System.out.println("Problemas ao alterar o Produto " + e);
+			  return false;
+		}
+		
+		return true;
+	}
+
+	public List<Eletronico> listarProdutos() {
+		List<Eletronico> lstProd = new ArrayList<>();
+		Connection con = Conexao.conectar();
+
 		// Listar os Produtos - inicio
-		String sql = "SELECT a.CODPRODUTO, a.NOMEPRODUTO, a.PRECOPRODUTO FROM produto a;";
+		String sql = "SELECT p.CODPRODUTO, p.NOMEPRODUTO, p.PRECOPRODUTO, e.MARCA, e.GARANTIAMESES ";
+		sql += "FROM produto p ";
+		sql += "INNER join eletronico e ";
+		sql += "ON p.CODPRODUTO = e.CODPRODUTO ";
+		sql += "ORDER BY p.CODPRODUTO;";
 		try {
 			PreparedStatement stm = con.prepareStatement(sql);
 			ResultSet rs = stm.executeQuery();
 			while (rs.next()) {
-				Produto p = new Produto();
-				p.setCodProduto(rs.getString("CODPRODUTO"));
-				p.setNomeProduto(rs.getString("NOMEPRODUTO"));
-				p.setPrecoProduto(rs.getDouble("PRECOPRODUTO"));
-				lstProd.add(p);
+				Eletronico e = new Eletronico();
+				e.setCodProduto(rs.getString("CODPRODUTO"));
+				e.setNomeProduto(rs.getString("NOMEPRODUTO"));
+				e.setPrecoProduto(rs.getDouble("PRECOPRODUTO"));
+				e.setMarca(rs.getString("MARCA"));
+				e.setGarantiaMeses(rs.getInt("GARANTIAMESES"));
+				lstProd.add(e);
 			}
-		} catch (Exception e){
-				System.out.println("Problemas ao listar os Produtos");
+		} catch (Exception e) {
+			System.out.println("Problemas ao listar os Produtos " + e);
 		}
 		return lstProd;
 	}
 
-	public Produto consultaProduto(){
+	public Produto consultaProduto(String pProd) {
 		Connection con = Conexao.conectar();
 		Produto p = new Produto();
 		p = null;
 		// Listar os Produtos - inicio
-		String sql  = "SELECT a.CODPRODUTO, a.NOMEPRODUTO, a.PRECOPRODUTO ";
-		       sql += "from produto a ";
-		       sql += "where codproduto = ? ";
+		String sql = "SELECT a.CODPRODUTO, a.NOMEPRODUTO, a.PRECOPRODUTO ";
+		sql += "from produto a ";
+		sql += "where codproduto = ? ";
 		try {
 			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setString(1, super.getCodProduto());
+			stm.setString(1, pProd);
 			ResultSet rs = stm.executeQuery();
 			while (rs.next()) {
 				p = new Produto();
@@ -99,12 +133,11 @@ public class Eletronico extends Produto {
 				p.setNomeProduto(rs.getString("NOMEPRODUTO"));
 				p.setPrecoProduto(rs.getDouble("PRECOPRODUTO"));
 			}
-		} catch (Exception e){
-				System.out.println("Problemas ao consultar o produto");
+		} catch (Exception e) {
+			System.out.println("Problemas ao consultar o produto");
 		}
 		return p;
 	}
-
 
 	public String getMarcaEletronico() {
 		return marcaEletronico;
@@ -113,8 +146,13 @@ public class Eletronico extends Produto {
 	public int getGarantiaMeses() {
 		return garantiaMeses;
 	}
-	
-	
-	
-	
+
+	public void setMarca(String marcaEletronico) {
+		this.marcaEletronico = marcaEletronico;
+	}
+
+	public void setGarantiaMeses(int garantiaMeses) {
+		this.garantiaMeses = garantiaMeses;
+	}
+
 }
--- ProdutoJDBC/src/produtojdbc/Principal.java
+++ ProdutoJDBC/src/produtojdbc/Principal.java
@@ -2,49 +2,78 @@ package produtojdbc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		String vCodProduto, vNomeProduto;
+		String vCodProduto, vNomeProduto, vMarca;
 		Double vPrecoProduto;
-		/*
-		vCodProduto   = JOptionPane.showInputDialog("Código");
-		vNomeProduto  = JOptionPane.showInputDialog("Nome");
-		vPrecoProduto = 1200.00;
+		int    vGarantiaMeses;
+		Scanner sc = new Scanner(System.in);
 		
-		Produto p1 = new Produto(vCodProduto,vNomeProduto, vPrecoProduto);
-		if (p1.incluirProduto()) {
+		// Entrada de dados
+		/*
+		System.out.print("Produto : ");
+		vCodProduto   =  sc.next();
+		System.out.print("Nome....: ");
+		vNomeProduto  =  sc.next();		
+		System.out.print("Preço...: ");
+		vPrecoProduto =  sc.nextDouble();
+		// eletronico
+		System.out.print("Marca...: ");
+		vMarca = sc.next();
+		System.out.print("Garantia: ");
+		vGarantiaMeses = sc.nextInt();
+		Eletronico e = new Eletronico(vCodProduto, vNomeProduto, vPrecoProduto, vMarca, vGarantiaMeses);
+		if (e.incluirProduto()) {
 			System.out.println("Produto incluído com sucesso");
 			
 		}
-		*/
-		
-		// listar produtos
-		
-		Eletronico e = new Eletronico();
+	*/
 		/*
-		List<Produto> listaProdutos = new ArrayList<>();
-		listaProdutos = e.listarProdutos();
-		if (listaProdutos != null) {
-			System.out.println("Codigo" + "\t" + "Nome" + "\t" + "Preco");			
-			for (Produto pr : listaProdutos) {
-				System.out.println(pr.getCodProduto() + "\t" + pr.getNomeProduto() + "\t" + pr.getPrecoProduto());			
-			}
-		} else {
-			System.out.println("Não foram localizados registros para listar");
-		}
-		*/
+		// listar produtos
+		Eletronico e1 = new Eletronico();
+		List<Eletronico> lista = new ArrayList();
+		lista = e1.listarProdutos();
+		System.out.printf("%-6s | %-15s |%-14s| %-13s | %-13s       ",
+				          "Codigo","Nome","Preço","Marca","Garantia");
+		System.out.println();
+	    for (Eletronico l : lista) {
+	       
+	        System.out.printf("%-6s | %-15s |R$ %10.2f | %-13s |",
+	        		          l.getCodProduto() , 
+	        		          l.getNomeProduto(), 
+	        		          l.getPrecoProduto(), 
+	        		          l.getMarcaEletronico());
+ 		    System.out.println(l.getGarantiaMeses());
+
+	    }		
+		
 		Produto p = new Produto();
-		p.setCodProduto("EL01");
-		p = e.consultaProduto();
+		var produto = "EL01";
+		p = e.consultaProduto(produto);
 		
 		if (p != null) {
 			System.out.println(p.getCodProduto() + " " + p.getNomeProduto() + " " + p.getPrecoProduto());
 		} else  {
 			System.out.println("Produto não cadastrado");
+		}
+		*/
+		
+		// alterar Produto
+		System.out.print("Produto : ");
+		vCodProduto   =  sc.next();
+		System.out.print("Nome....: ");
+		vNomeProduto  =  sc.next();		
+		System.out.print("Preço...: ");
+		vPrecoProduto =  sc.nextDouble();
+		Produto p = new Produto(vCodProduto, vNomeProduto, vPrecoProduto);
+		Eletronico e = new Eletronico();
+		if (e.alterarProduto()) {
+			System.out.println("Produto alterado com sucesso");
 		}
 	}
 
